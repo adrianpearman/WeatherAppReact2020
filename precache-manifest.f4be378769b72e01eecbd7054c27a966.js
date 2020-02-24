7ed71b6cc945a2fc1e8f6c0f7cec63f5de5a1c91
Adrian Pearman <adrianpearman12@gmail.com>
2020-02-24 02:16:46 -0500

    Updates

---
+++ precache-manifest.f4be378769b72e01eecbd7054c27a966.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cfbc2922bf8b0a57f158b2d21693f28",
+    "url": "/WeatherAppReact2020/index.html"
+  },
+  {
+    "revision": "01924889ae8fe70d1adc",
+    "url": "/WeatherAppReact2020/static/css/main.f54f9a6c.chunk.css"
+  },
+  {
+    "revision": "77802d5742e3ba609965",
+    "url": "/WeatherAppReact2020/static/js/2.e1aa7d5b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/WeatherAppReact2020/static/js/2.e1aa7d5b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01924889ae8fe70d1adc",
+    "url": "/WeatherAppReact2020/static/js/main.910c3557.chunk.js"
+  },
+  {
+    "revision": "ab00825301129c14e3ac",
+    "url": "/WeatherAppReact2020/static/js/runtime-main.744bcd95.js"
+  },
+  {
+    "revision": "577719e9822ff30fdce9220699d2908c",
+    "url": "/WeatherAppReact2020/static/media/cold.577719e9.png"
+  },
+  {
+    "revision": "d3cbea7bc4d3b57c0293c7e8ea1a51dd",
+    "url": "/WeatherAppReact2020/static/media/warm.d3cbea7b.png"
+  }
+]);
